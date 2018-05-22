4df2b8143356b6baceed53518547f481b2b4ec81
Gabor Janvari (MAC) <janvari.gabor2@gmail.com>
2018-05-22 20:33:01 +0200

    A 3.) sor Macen, de közben a DELL-en is szerkesztem.

--- src/git_gyakorlas/Ki_iro.java
+++ src/git_gyakorlas/Ki_iro.java
@@ -5,6 +5,7 @@ public class Ki_iro {
 	public static void main(String[] args) {
 		System.out.println("1.) Első módosítás, első sor"); 
 		System.out.println("2.) Második sor, UTF-8");
+		System.out.println("3.) A harmadik sor Mac-en pedig EZ.");
 		
 
 	}
