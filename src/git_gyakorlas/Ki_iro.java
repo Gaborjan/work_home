f152fe04619bea8bebcbcd431f67d3d946fbd2fe
Gabor Janvari (MAC) <janvari.gabor2@gmail.com>
2018-05-22 20:50:58 +0200

    Irtam egy fantasztikus csillagrajzoló metódust! :)

--- src/git_gyakorlas/Ki_iro.java
+++ src/git_gyakorlas/Ki_iro.java
@@ -3,11 +3,14 @@ package git_gyakorlas;
 public class Ki_iro {
 
 	public static void main(String[] args) {
+		csillagiro();
 		System.out.println("1.) Első módosítás, első sor"); 
 		System.out.println("2.) Második sor, UTF-8");
 		System.out.println("3.) A harmadik sor Mac-en pedig EZ.");
-		
-
+		csillagiro();
+	}
+	public static void csillagiro() {
+		System.out.println("*****************************************");
 	}
 
 }
